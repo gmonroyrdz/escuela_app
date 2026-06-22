ea369a2ce206e84058a0bf2731b87bf0710ae9b5
Guillermo Monroy <gmonroy@cua.uam.mx>
2026-06-22 09:42:52 -0600

    Primera sentecia de SQL sobre MySQL

--- src/main/java/mx/uam/cua/Main.java
+++ src/main/java/mx/uam/cua/Main.java
@@ -1,15 +1,60 @@
 package mx.uam.cua;
 
-import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Main {
     public static void main(String[] args) {
-        Connection conn = ConexionBD.conectar();
-        
-        if(conn != null){
-            System.out.println("La conexión se realizó correctamente.");
-        }else{
-            System.out.println("No se pudo conectar.");
+        ConexionBD conexion = new ConexionBD("localhost", "3306", "escuela", "usr_escuela", "p455W0rd!");
+
+        Statement sentencia = null;
+        ResultSet resultado = null;
+        conexion.conectar();
+
+        try {
+            sentencia = conexion.getConexion().createStatement();
+            String sql = "SELECT id, nombre, apellido FROM estudiantes";
+            resultado = sentencia.executeQuery(sql);
+
+            while (resultado.next()) {
+                int id = resultado.getInt("id");
+                String nombre = resultado.getString("nombre");
+                String apellido = resultado.getString("apellido");
+                
+                System.out.println(id + " - "+ nombre+ " - "+ apellido);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally{
+            try {
+                if(resultado != null){
+                    resultado.close();
+                }
+            } catch (SQLException e) {
+                System.err.println(e.getMessage());
+            }
+            try{
+                if(sentencia != null){
+                    sentencia.close();
+                }
+            }catch(SQLException e){
+                System.err.println(e.getMessage());          
+            }
+            try {
+                if(conexion != null){
+                    conexion.getConexion().close();
+                }
+            } catch (SQLException e) {
+                System.err.println(e.getMessage());
+            }
         }
+
+
+
+        
+        
+
+
     }
 }
